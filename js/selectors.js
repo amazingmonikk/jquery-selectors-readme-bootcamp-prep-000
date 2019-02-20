d1536329b1e418530ce1969ff834b8ced79d7328
Monika Tadross <monikk.tadross@gmail.com>
2019-02-20 23:05:02 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,4 +1,9 @@
 // declare your functions here...
+$(document).ready(function() {
+  $("#text").append("this is so freaking awesome!!!.");
+  });
+
+
 function paragraphSelector() {
   var count = 0;
   const p = paragraph;
